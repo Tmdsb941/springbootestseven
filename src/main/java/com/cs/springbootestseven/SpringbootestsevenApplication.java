f799924dba8a26cd1fa6073d8fed41f476885739
Tmdsb941 <1927919399@qq.com>
2018-12-18 14:54:54 +0800

    Initial commit

---
+++ src/main/java/com/cs/springbootestseven/SpringbootestsevenApplication.java
@@ -0,0 +1,14 @@
+package com.cs.springbootestseven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootestsevenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootestsevenApplication.class, args);
+    }
+
+}
+
